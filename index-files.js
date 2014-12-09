8527fd654d3c2e3e6f7d9114c907e0f4293932d5
kliment <kliment.lambevski@gmail.com>
2014-12-09 19:02:04 +0100

    file blocks

--- index-files.js
+++ index-files.js
@@ -7,6 +7,34 @@ var Db = require('mongodb').Db,
 var document=[];
 var calls=[];
 console.time('indexing');
+function readFiles(files,bulk,currentFileBlock,fileBlocks,db){
+    async.each(files.slice(currentFileBlock*1000,currentFileBlock*1000+1000),function(file,callback){
+        fs.readFile('downloaded-files/'+file, 'utf-8', function(err, contents) {
+            if(!err){
+                console.log(this.fileName);
+                bulk.insert(indexFiles(contents));
+                callback();
+            }
+        }.bind({fileName:file}));
+    },function(err){
+        if( err ) { return console.log(err); }
+        bulk.execute(function(err,result){
+            if(err){
+                console.log(err);
+            }else{
+                console.log('uspesno');
+                console.log(currentFileBlock,fileBlocks);
+                if(currentFileBlock<fileBlocks)
+                  readFiles(files,bulk,currentFileBlock+1,fileBlocks,db);
+                else{
+                    db.close();
+                }
+            }
+
+        });
+        console.timeEnd('indexing');
+    });
+}
 MongoClient.connect("mongodb://localhost:27017/searchdb", function(err, db) {
     if(!err) {
         var collection=db.collection('indexes');
@@ -18,26 +46,8 @@ MongoClient.connect("mongodb://localhost:27017/searchdb", function(err, db) {
         var bulk=collection.initializeUnorderedBulkOp();
         fs.readdir('downloaded-files', function(err, files) {
             var htmlpages=files.filter(function(file) { return file.substr(-5) === '.html'; });
-            async.each(htmlpages.slice(0,10000),function(file,callback){
-                fs.readFile('downloaded-files/'+file, 'utf-8', function(err, contents) {
-                    if(!err){
-                        console.log(this.fileName);
-                        bulk.insert(indexFiles(contents));
-                        callback();
-                    }
-                }.bind({fileName:file}));
-            },function(err){
-                if( err ) { return console.log(err); }
-                bulk.execute(function(err,result){
-                    if(err){
-                        console.log(err);
-                    }else{
-                        console.log('uspesno');
-                    }
-                    db.close();
-                });
-                console.timeEnd('indexing');
-            });
+            var fileBlocks=htmlpages.length/1000;
+            readFiles(htmlpages,bulk,0,fileBlocks,db);
         });
     } else {
         console.log("fail");
